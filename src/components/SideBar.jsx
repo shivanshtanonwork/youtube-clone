10a68d366f4dc109531e8b1dbf6d022a5c242faa
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-03-01 14:37:37 +0530

    setup redux store

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="p-5 shadow-lg w-48">
+      <ul>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+      </ul>
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+      </ul>
+      <h1 className="font-bold pt-5">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
